6819259fb244b4cb4c7b3750c53334316d22e603
Bootcamp <bootcamp@Abduls-Air.hsd1.va.comcast.net>
2023-06-01 00:57:42 -0700

    Added styling to Navigation Bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,15 @@
 import React from "react"
 import profile from "/Users/bootcamp/bootcamp/My-React-Portfolio/my-react-portfolio/src/profile.jpg"
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
+import {faBars} from "@fortawesome/free-solid-svg-icons"
+
 
 export const Navbar = ({setCurrentPage}) => {
     return (
             <nav className="navbar navbar-expand-lg navbar-light bg-dark">
                 <a className="navbar-brand" href="#"><img className="profile" src={profile} alt="Profile Picture" /> <span className="name">Abdul Basit</span> </a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
+                    <FontAwesomeIcon icon = {faBars} style= {{ color: "lightblue"}}/>
                 </button>
 
                 <div className="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
